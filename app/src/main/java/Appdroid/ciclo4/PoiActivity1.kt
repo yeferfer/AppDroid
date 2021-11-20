e304c6c8fc686e37121ff26b6cf64032c53de3db
ebernaguz26 <ebernaguz26@gmail.com>
2021-11-20 13:31:02 -0500

    Organizacion MainActivity, Creacion Activities para 5 POI, Programacion botones POI y Volver

---
+++ app/src/main/java/Appdroid/ciclo4/PoiActivity1.kt
@@ -0,0 +1,18 @@
+package Appdroid.ciclo4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class PoiActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poi1)
+        val btninicio = findViewById<Button>(R.id.btnVolver1)
+        btninicio.setOnClickListener {
+            val regresa = Intent(this, MainActivity::class.java)
+            startActivity(regresa)
+        }
+    }
+}
