b2f70e260181686f06d70889e38445f2cce530f0
al_prieto <alejandroprieto.has@gmail.com>
2021-11-24 08:49:58 -0500

    Create RecyclerView

---
+++ app/src/main/java/Appdroid/ciclo4/CustomAdapter.kt
@@ -0,0 +1,57 @@
+package Appdroid.ciclo4
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+
+    val titles = arrayOf("AppDroid",
+    "Suscribete",
+    "Videos",
+    "Youtube")
+
+    val details = arrayOf("Muchos videos nuevos",
+        "Kotlin",
+        "Más videos",
+        "Gran cantidad de videos")
+
+    val images = intArrayOf(R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground)
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemDetail.text = details[i]
+        viewHolder.itemImage.setImageResource(images[i])
+
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_description)
+        }
+    }
+}
