ca7ecc992ca9ddb274b069f18849973da6c44fab
yeferfer <jefer.1142@gmail.com>
2021-12-19 20:46:02 -0500

    Json BD agregado

--- app/src/main/java/Appdroid/ciclo4/MainActivity.kt
+++ app/src/main/java/Appdroid/ciclo4/MainActivity.kt
@@ -1,8 +1,10 @@
 package Appdroid.ciclo4
 
+import Appdroid.ciclo4.Model.PoiModelClass
 import Appdroid.ciclo4.archivojson.ControladorArchivoJson
 import Appdroid.ciclo4.datos.InfoPoi
 import android.content.Intent
+import android.media.audiofx.AudioEffect
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.WindowManager
@@ -14,13 +16,18 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
+import org.json.JSONException
+import org.json.JSONObject
 import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toggle: ActionBarDrawerToggle
 
+    //Archivos Json Local
     private var listaPoi = mutableListOf<InfoPoi>()
     private val nombreArchivo = "pois.txt"
     private val controlJson = ControladorArchivoJson()
@@ -37,38 +44,34 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        //Informacion archivo Json
+        //Archivo Json
         archivo = File(this.applicationContext.filesDir, nombreArchivo)
         if (archivo!!.exists()) {
             archivo!!.createNewFile()
         }
+        val usersList: ArrayList<PoiModelClass> = ArrayList()
+        try {
+            val obj = JSONObject(getJSONFromAssets()!!)
+            val usersArray = obj.getJSONArray("poi")
 
-        listaPoi.addAll(
-            arrayListOf(
-                (InfoPoi(
-                    "STATUE OF LIBERTY",
-                    "Nueva York cuenta con una temperatura aproximada de 6 Cº, incluye 5 distritos que se ubican donde el río Hudson desemboca en el océano Atlántico. En su centro se encuentra Manhattan, un distrito densamente poblado que se encuentra entre los principales centros comerciales, financieros y culturales del mundo. Sus sitios icónicos incluyen rascacielos, como el Empire State Building, y el amplio Central Park. El teatro Broadway se ubica en la zona del Times Square iluminada con neones."
-                )),
-                (InfoPoi(
-                    "MUSEUM MADAME TUSSAUNDS",
-                    "El Museo Madame Tussauds es el museo de cera más conocido en el mundo. Posee la colección más grande de figuras de celebridades. La sede central del museo está en Londres, pero también hay establecimientos en otras grandes ciudades alrededor del mundo."
-                )),
-                (InfoPoi(
-                    "ONE WORLD OBSERVATORY",
-                    "Situado en la parte baja de Manhattan, el Observatorio One World presenta un mirador de última generación a 102 pisos de altura. Viaja directamente a su cima en 47 segundos en el ascensor SkyPod y profundiza en la historia cultural y financiera de la ciudad con una serie de exposiciones interactivas e interesantes charlas dirigidas por los embajadores del tour."
-                )),
-                (InfoPoi(
-                    "911 MUSEUM",
-                    "El centro está ubicado en el sitio del World Trade Center, en la misma ubicación de las Torres Gemelas destruidas en los ataques del 11 de septiembre de 2001. El World Trade Center Memorial Foundation cambió de nombre a National September 11 Memorial & Museum at the World Trade Center en 2007. El ganador del diseño para la competencia del World Trade Center Site Memorial fue el arquitecto israelí-estadounidense Michael Arad de Handel Architects, una firma de San Francisco y Nueva York. Arad trabajó con la firma de arquitectura paisajista Peter Walker and Partners en el diseño en el que lo llaman 'bosque de árboles' con dos piscinas gemelas en el centro, en los cimientos donde alguna vez estuvieron las Torres Gemelas."
-                )),
-                (InfoPoi(
-                    "EDGE",
-                    "Edge es la nueva y emocionante atracción turística de la ciudad de Nueva York, situada en lo alto del fabuloso y reluciente rascacielos Hudson Yards. Subirás 100 pisos en su moderno ascensor y, después, podrás salir al aire libre en su amplio mirador para deleitarte con las vistas del horizonte más famoso del mundo."
-                ))
-            )
-        )
-        controlJson.guardarJsonEnArchivo(archivo!!, listaPoi)
+            for (i in 0 until usersArray.length()) {
+                val user = usersArray.getJSONObject(i)
+                val id = user.getInt("id")
+                val titulo = user.getString("titulo")
+                val Descricion = user.getString("Descricion")
+                val userDetails = PoiModelClass(id, titulo, Descricion)
+                listaPoi.add(
+                    InfoPoi(titulo, Descricion)
+                )
+                usersList.add(userDetails)
+                controlJson.guardarJsonEnArchivo(archivo!!, listaPoi)
+            }
 
+        } catch (e: JSONException) {
+            //exception
+            println("Esta en error")
+            e.printStackTrace()
+        }
 
         //Funcion de RecyclerView
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
@@ -124,8 +127,29 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+
     }
 
+    //ArchivoJson
+    private fun getJSONFromAssets(): String? {
+
+        var json: String? = null
+        val charset: Charset = Charsets.UTF_8
+        try {
+            val myUsersJSONFile = assets.open("infopoi.json")
+            val size = myUsersJSONFile.available()
+            val buffer = ByteArray(size)
+            myUsersJSONFile.read(buffer)
+            myUsersJSONFile.close()
+            json = String(buffer, charset)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return null
+        }
+        return json
+    }
+
+    //Menu Hamburguesa
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) {
             return true
@@ -134,3 +158,5 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
+
---
+++ app/src/main/java/Appdroid/ciclo4/Model/PoiModelClass.kt
@@ -0,0 +1,8 @@
+package Appdroid.ciclo4.Model
+
+class PoiModelClass(
+    val id: Int,
+    val titulo: String,
+    val Descricion: String
+
+)
